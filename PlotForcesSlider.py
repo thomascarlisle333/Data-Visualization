03521f6cf6df112c77ed283728442b3f30d14e2e
thomascarlisle333 <thomascarlisle333@gmail.com>
2025-02-06 08:40:10 -0500

    Fixed X Axis Indexing

--- PlotForcesSlider.py
+++ PlotForcesSlider.py
@@ -7,8 +7,8 @@ file_path_data = r"C:\Users\tcarlisle\Documents\Python\Plotting Data\run02.csv" 
 file_path_control = r"C:\Users\tcarlisle\Documents\Python\Plotting Data\cuttercycling.csv"  # Control data file path
 
 # Read the CSV files
-df_data = pd.read_csv(file_path_data)
-df_control = pd.read_csv(file_path_control)
+df_data = pd.read_csv(file_path_data, index_col = 'Time')
+df_control = pd.read_csv(file_path_control, index_col = 'Time')
 
 # Ensure the length of the two dataframes match by aligning them
 min_len = min(len(df_data), len(df_control))
@@ -33,13 +33,17 @@ df_control["Fy_lb"] = df_control["Fy"] * 0.224809 - 3 + 6.6
 df_control["Fz_lb"] = df_control["Fz"] * 0.224809 - 46.1 + 41.8
 
 # Convert Moments from Nm to lb*in (for data and control)
-df_data["Tx_lb_in"] = df_data["Tx"] * 0.224809 * 12 - 8
-df_data["Ty_lb_in"] = df_data["Ty"] * 0.224809 * 12 + 78
-df_data["Tz_lb_in"] = df_data["Tz"] * 0.224809 * 12 - 25
+df_data["Tx_lb_in"] = df_data["Tx"] * 8.8507 
+df_data["Ty_lb_in"] = df_data["Ty"] * 8.8507 + 200
+df_data["Tz_lb_in"] = df_data["Tz"] * 8.8507 
 
-df_control["Tx_lb_in"] = df_control["Tx"] * 0.224809 * 12 - 8 
-df_control["Ty_lb_in"] = df_control["Ty"] * 0.224809 * 12 + 78
-df_control["Tz_lb_in"] = df_control["Tz"] * 0.224809 * 12 - 25 + 17
+print(df_data.describe())
+
+df_control["Tx_lb_in"] = df_control["Tx"] * 8.8507 
+df_control["Ty_lb_in"] = df_control["Ty"] * 8.8507 
+df_control["Tz_lb_in"] = df_control["Tz"] * 8.8507 
+
+print(df_control.describe())
 
 # Set up the plot with extra space between the two subplots
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
@@ -79,10 +83,10 @@ plt.subplots_adjust(bottom=0.2, hspace=0.35)  # Increase hspace for more space b
 
 # Sliders for adjusting the start and end index
 ax_slider_start = plt.axes([0.2, 0.02, 0.65, 0.03], facecolor='lightgoldenrodyellow')
-slider_start = Slider(ax_slider_start, 'Start Index', 0, min_len-1, valinit=0, valstep=1)
+slider_start = Slider(ax_slider_start, 'Start Index', 0, min_len-1, valinit=0, valstep=.1)
 
 ax_slider_end = plt.axes([0.2, 0.06, 0.65, 0.03], facecolor='lightgoldenrodyellow')
-slider_end = Slider(ax_slider_end, 'End Index', 0, min_len-1, valinit=min_len-1, valstep=1)
+slider_end = Slider(ax_slider_end, 'End Index', 0, min_len-1, valinit=min_len-1, valstep=.1)
 
 # Button functions to toggle visibility of each component (force and moment)
 def toggle_fx_data(event):
@@ -208,28 +212,29 @@ def update(val):
     start_index = int(slider_start.val)
     end_index = int(slider_end.val)
     
-    # Update lines for forces and moments
-    line_fx_data.set_data(df_data.index[start_index:end_index], df_data["Fx_lb"][start_index:end_index])
-    line_fy_data.set_data(df_data.index[start_index:end_index], df_data["Fy_lb"][start_index:end_index])
-    line_fz_data.set_data(df_data.index[start_index:end_index], df_data["Fz_lb"][start_index:end_index])
+    # Use .iloc to slice the DataFrame based on positional index
+    line_fx_data.set_data(df_data.index[start_index:end_index], df_data["Fx_lb"].iloc[start_index:end_index])
+    line_fy_data.set_data(df_data.index[start_index:end_index], df_data["Fy_lb"].iloc[start_index:end_index])
+    line_fz_data.set_data(df_data.index[start_index:end_index], df_data["Fz_lb"].iloc[start_index:end_index])
     
-    line_fx_control.set_data(df_control.index[start_index:end_index], df_control["Fx_lb"][start_index:end_index])
-    line_fy_control.set_data(df_control.index[start_index:end_index], df_control["Fy_lb"][start_index:end_index])
-    line_fz_control.set_data(df_control.index[start_index:end_index], df_control["Fz_lb"][start_index:end_index])
+    line_fx_control.set_data(df_control.index[start_index:end_index], df_control["Fx_lb"].iloc[start_index:end_index])
+    line_fy_control.set_data(df_control.index[start_index:end_index], df_control["Fy_lb"].iloc[start_index:end_index])
+    line_fz_control.set_data(df_control.index[start_index:end_index], df_control["Fz_lb"].iloc[start_index:end_index])
     
-    line_tx_data.set_data(df_data.index[start_index:end_index], df_data["Tx_lb_in"][start_index:end_index])
-    line_ty_data.set_data(df_data.index[start_index:end_index], df_data["Ty_lb_in"][start_index:end_index])
-    line_tz_data.set_data(df_data.index[start_index:end_index], df_data["Tz_lb_in"][start_index:end_index])
+    line_tx_data.set_data(df_data.index[start_index:end_index], df_data["Tx_lb_in"].iloc[start_index:end_index])
+    line_ty_data.set_data(df_data.index[start_index:end_index], df_data["Ty_lb_in"].iloc[start_index:end_index])
+    line_tz_data.set_data(df_data.index[start_index:end_index], df_data["Tz_lb_in"].iloc[start_index:end_index])
     
-    line_tx_control.set_data(df_control.index[start_index:end_index], df_control["Tx_lb_in"][start_index:end_index])
-    line_ty_control.set_data(df_control.index[start_index:end_index], df_control["Ty_lb_in"][start_index:end_index])
-    line_tz_control.set_data(df_control.index[start_index:end_index], df_control["Tz_lb_in"][start_index:end_index])
+    line_tx_control.set_data(df_control.index[start_index:end_index], df_control["Tx_lb_in"].iloc[start_index:end_index])
+    line_ty_control.set_data(df_control.index[start_index:end_index], df_control["Ty_lb_in"].iloc[start_index:end_index])
+    line_tz_control.set_data(df_control.index[start_index:end_index], df_control["Tz_lb_in"].iloc[start_index:end_index])
 
     # Update x-axis limits to match the visible range of the data
     ax1.set_xlim(df_data.index[start_index], df_data.index[end_index])
     ax2.set_xlim(df_data.index[start_index], df_data.index[end_index])
     
     fig.canvas.draw_idle()
+
 
 # Attach update function to sliders
 slider_start.on_changed(update)
